1905b61bdd6c9fcbea6b8ecde1b787a40bda71c3
chenjiinguyen <duyntp2000@gmail.com>
2021-04-21 13:11:50 +0700

    style:create navigation bottom bar

--- app/src/main/java/com/chenjinguyen/bookcommunity/MainActivity.java
+++ app/src/main/java/com/chenjinguyen/bookcommunity/MainActivity.java
@@ -2,7 +2,10 @@ package com.chenjinguyen.bookcommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.chenjinguyen.bookcommunity.activity.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent t = new Intent(this, HomeActivity.class);
+        startActivity(t);
     }
 }
---
+++ app/src/main/java/com/chenjinguyen/bookcommunity/activity/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.chenjinguyen.bookcommunity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.chenjinguyen.bookcommunity.R;
+import com.chenjinguyen.bookcommunity.fragment.HomeFragment;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        loadFragment(new HomeFragment());
+    }
+
+    public void loadFragment(Fragment frm){
+        FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+        trans.replace(R.id.frame_home,frm);
+        trans.commit();
+    }
+}
